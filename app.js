bd02c818212a8d76c1af57778cdd3ef3568d629f
Rajendhar Mamidala <rajendhar.mamidala@capgemini.com>
2017-04-30 12:52:16 +0530

    Calculate sum of 1 to 1000 numbers examples

---
+++ app.js
@@ -0,0 +1,23 @@
+// For Loop - Calculate sum of 1 to 1000 number.
+var fsumOfNumbers = 0; // Variable to store sum of numbers.
+
+// For Loop - to iterate through 1 to 1000 numbers.
+for (var i =1;i<=1000; i++) {
+	fsumOfNumbers = fsumOfNumbers + i;
+}
+
+// Print the sum of all the sumbers.
+console.log(fsumOfNumbers);
+
+// While Loop - Calculate sum of 1 to 1000 number.
+var wsumOfNumbers = 0; // Variable to store sum of numbers.
+var j = 1; // Initial value of the while counter.
+
+// While Loop - to iterate through 1 to 1000 numbers.
+while(j <= 1000) {
+	wsumOfNumbers = wsumOfNumbers + j;
+  j++;
+}
+
+// Print the sum of all the sumbers.
+console.log(wsumOfNumbers);
